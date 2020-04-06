2b3c14904d9e14590ffa25c8e1f933ad4029cd03
kieransmith <kieransmith@uvic.ca>
2020-04-06 12:20:24 -0700

    Redid most of everything

--- modupdate.py
+++ modupdate.py
@@ -14,104 +14,158 @@ import re
 import requests
 import wget
 import os
+from termcolor import colored
 
 modfile = "modlist.txt"
 trackerfile = "modtracker.txt"
-version = "1.14"
+versionparam = ["1.14.4", "Forge"] # Preferred terms should go first
 exreg = r"([^\/]*)$"
 apithing = "https://api.cfwidget.com/minecraft/mc-mods/"
 modDir = "mods" # Don't use ./
 
-missingthing = []
-
 
 def main():
 	print("Gathering important stuff, this might take a while")
-	links = getLinkdict(modfile)
+	mods = readModLines(modfile)
+	nameLinks = getNameLinks(mods, versionparam)
+	pprint(nameLinks)
+	downloadMods(nameLinks)
+
+	print("\nDone!")
+
+def downloadMods(nameLinks):
+	# Make mod dir if not exist
+	if not os.path.exists(modDir):
+		os.mkdir(modDir)
 
 	# Load tracker
 	oldlinks = {}
 	if os.path.isfile(trackerfile):
 		with open(trackerfile) as j:
 			oldlinks = json.load(j)
-	
-	# Make mod dir if not exist
-	if not os.path.exists(modDir):
-		os.mkdir(modDir)
-	
+
 	# Download loop
-	for link in links.keys():
+	for link in nameLinks.keys():
 		loc =  modDir + "/%s.jar" % link
 		if link in oldlinks.keys():
-			if links[link] == oldlinks[link]:
+			if nameLinks[link] == oldlinks[link]:
 				continue # Skip if not new and no update available
 			else:
 				print("Updating %s" % link)
 				os.remove(loc) # Remove old mod
 		else:
 			print("Downloading new mod %s" % link) # New mod
-		getFile(links[link], loc) # Download the stuff
+		downloadFile(nameLinks[link], loc) # Download the stuff
 
 	# Update tracker
 	with open(trackerfile, "w") as t:
-		json.dump(links, t)
-
-	print()
-	for missing in missingthing:
-		print("We missed %s" % missing)
-
-	print("\nDone!")
+		json.dump(nameLinks, t)
 
 
-def getFile(url, out):
+# Downloads a file
+def downloadFile(url, out):
 	print("DL: %s" % url)
 	wget.download(url, out)
 	print()
 
 
-def getLinkdict(file):
-	theFile = open(file)
+# Returns a list of mods to be downloaded
+def readModLines(path):
+	theFile = open(path)
 	contents = theFile.readlines()
 	theFile.close()
-	# Build the name and id dict
-	nameIdDict = {}
+	filtered = []
 	for line in contents:
-		# Skip if is comment
 		if line.startswith("#") or not line.strip():
 			continue 
-		# Prepare name
-		name = re.search(exreg, line, re.M|re.I).group(1).strip()
-		
-		print("Finding %s" % name)
-		
-		# Prepare id
-		link = apithing + name
-		if version != "":
-			link += "?version=" + version
-		
-		print("Getting data from %s" % link)
+		line = re.search(exreg, line, re.M|re.I).group(1).strip()
+		filtered.append(line)
+	return filtered
 
+
+# It's easy
+def errorMsg(msg):
+	print(colored(msg, 'red'))
+
+
+# Gets the links to the files
+# Returns a dictionary mapping the names to the links
+def getNameLinks(modlist, version):
+	# Build the name and id dict
+	nameIdDict = {}
+	for mod in modlist:
+		print("Finding %s" % mod)
+		# Prepare id
+		link = apithing + mod
+		#print("Getting data from %s" % link)
 		jdata = getJSON(link)
 		if "error" in jdata.keys():
-			print("NOT FOUND THING I HATE THIS")
-			textyyy = name + " - " + jdata["error"]
-			missingthing.append(textyyy)
+			print("")
+			textyyy = mod + " - " + jdata["error"]
+			errorMsg("Could not find %s" % textyyy)
 			continue
-
-
-		modid = jdata["id"]
-		fileid = jdata["download"]["id"]
-
-		protodirectlink = "https://addons-ecs.forgesvc.net/api/v2/addon/%s/file/%s/download-url" % (modid, fileid) # The link that gets the link
+		# Make entry
+		mid = jdata["id"]
+		fid = getFileId(jdata["files"], version)
+		# Retrieve the actual download link
+		protodirectlink = "https://addons-ecs.forgesvc.net/api/v2/addon/%s/file/%s/download-url" % (mid, fid) # The link that gets the link
 		headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'} # Required to prevent 403
 		directlink = requests.get(protodirectlink, headers=headers).text
-		# Make entry
-		nameIdDict[name] = directlink
+		nameIdDict[mod] = directlink
+		
 	return nameIdDict
 
+
+# Finds a link to a mod file based on version specs
+def getFileId(files, version):
+	fileid = 0 # should use date uploaded
+	outerAccuracy = 0
+	outerFid = 0
+	for release in ["release", "beta", "alpha"]:
+		accuracy = 0
+		for filey in files:
+			if filey["type"] != release:
+				continue # Skip if not matching the release type
+			fireversions = filey["versions"]
+			newAccuracy = 0
+
+			#print("\tScanning %s" % filey["name"])
+			bonus = 1+len(version) # Prefer the first params because I hate this
+			for v in version:
+				bonus -= 1
+				if v in fireversions:
+					newAccuracy += bonus
+					#print("\t\tAdding %s because it has %s, total is %i" % (bonus, v, newAccuracy))
+					continue # Don't count multiple times
+				
+			#print(fireversions, " - ", newAccuracy)
+			if newAccuracy > accuracy:
+				fileid = filey["id"]
+				accuracy = newAccuracy
+			elif newAccuracy == accuracy and filey["id"] > fileid:
+				fileid = filey["id"]
+				accuracy = newAccuracy
+		if accuracy == adapt(len(version)): # I HATE THIS
+			print("\tPerfectly matched %s with id %s with accuracy %i/%i" % (release, fileid, accuracy, adapt(len(version))))
+			return fileid # It's as good as we will find
+		elif fileid != 0:
+			print("\tMatched %s with id %s with accuracy %i/%i" % (release, fileid, accuracy, len(version)))
+			if outerAccuracy < accuracy:
+				print("\treplacing outer")
+				outerAccuracy = accuracy
+				outerFid = fileid
+		else:
+			print("\tNo %s found" % release)
+	return outerFid
+	
+
+# It gets JSON data
 def getJSON(url):
 	r = requests.get(url)
 	return r.json()
+
+def adapt(n):
+	return n*(n+1)/2
 
 
 if __name__ == "__main__":
